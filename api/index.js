2b37ff7ac594dda4e3f13e7e64c342401dbaedfb
pablo-salituri <pablo_salituri@live.com.ar>
2023-03-14 12:19:12 -0300

    14-03-22

--- api/index.js
+++ api/index.js
@@ -19,9 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const {getAndSavePokemons} = require('./src/controllers/Pokemon/getPokemons.js')  //Agregado por mí
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
+  getAndSavePokemons();                   // Agregado por mí
+  console.log('Database Cargada');        // Agregado por mí
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
