20c34466b0e45392f2be7124be2fb42e1a4ee431
pablo-salituri <pablo_salituri@live.com.ar>
2023-03-15 18:27:27 -0300

    15-03-23

--- api/src/controllers/Pokemon/postPokemons.js
+++ api/src/controllers/Pokemon/postPokemons.js
@@ -1,20 +1,33 @@
-const axios = require('axios');
+// const axios = require('axios');         Es de la parte comentada del código
 const URL = 'https://pokeapi.co/api/v2/pokemon/';
 const  {Pokemon}  = require('../../db.js');
-var i = 1;
+//var i = 1;    Es de la parte comentada del código
 
 
 const postPokemons = async(ID, Nombre, Imagen, Vida, Ataque, Defensa, Velocidad, Altura, Peso) => {
     try {
         //Valido si el id existe en la BDD
-        const respuestaDataBase = await Pokemon.findOne({
-            where: {
-                ID
-            }
-        })
-        if (respuestaDataBase)
+        if (await Pokemon.findOne({
+            where: {ID}
+            })
+        )
             return {error: 'No se pudo completar la carga porque ya existe un Pokemon con ese Id'}
+        
 
+        //Valido si el Nombre existe en la BDD
+        if (await Pokemon.findOne({
+            where: {Nombre}
+            })
+        )
+            return {error: 'No se pudo completar la carga porque ya existe un Pokemon con ese Nombre'}
+
+
+        //Valido si La imagen existe en la BDD
+        if (await Pokemon.findOne({
+            where: {Imagen}
+            })
+        )
+            return {error: 'No se pudo completar la carga porque ya existe un Pokemon con esa Imagen'}
 
         // * Esta es la busqueda de id en Api. Funciona bien, pero tarda muchísimo
         /* Valido si el Id existe en la Api
@@ -57,14 +70,49 @@ module.exports = {postPokemons}
 
 
 
+/* 
+// const axios = require('axios');         Es de la parte comentada del código
+const URL = 'https://pokeapi.co/api/v2/pokemon/';
+const  {Pokemon}  = require('../../db.js');
+//var i = 1;    Es de la parte comentada del código
 
-/* const postPokemons = async(ID, Nombre, Imagen, Vida, Ataque, Defensa, Velocidad, Altura, Peso) => {
+
+const postPokemons = async(ID, Nombre, Imagen, Vida, Ataque, Defensa, Velocidad, Altura, Peso) => {
     try {
+        //Valido si el id existe en la BDD
+        const respuestaDataBase = await Pokemon.findOne({
+            where: {
+                ID,
+            }
+        })
+        if (respuestaDataBase)
+            return {error: 'No se pudo completar la carga porque ya existe un Pokemon con ese Id'}
+
+
+        // * Esta es la busqueda de id en Api. Funciona bien, pero tarda muchísimo
+        /* Valido si el Id existe en la Api
+        Busco el total de pokemons en la Api
+        const respuesta = await axios.get(URL);
+        const totalDePokemon = respuesta.data.count
+        
+
+        Itero hasta que termine o encuentre uno con ese Id
+        do {
+            const response = await axios.get(`${URL}${i}`);
+            const data = response.data.id;
+            console.log('id por api  ' + data);
+            if (ID === data)
+                return {error: 'No se pudo completar la carga porque ya existe un Pokemon con ese Id en la Api'}
+            i++
+        }
+        while(i <= totalDePokemon) 
+
+        
         const newPokemon = await Pokemon.create({
             ID,
             Nombre,
-            Imagen,
-            Vida,
+            Imagen,                 ToDo: Debe crear un pokemon en la base de datos, y este debe
+            Vida,                   ToDo: estar relacionado con sus tipos indicados (al menos uno).
             Ataque,
             Defensa,
             Velocidad,
@@ -75,4 +123,7 @@ module.exports = {postPokemons}
     } catch (error) {
         return {error: 'No se pudo agregar el pokemon solicitado'}
     }
-} */
+}
+
+
+module.exports = {postPokemons} */
