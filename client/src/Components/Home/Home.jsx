7385afa940ece33219e82d6070f74e343c92a430
pablo-salituri <pablo_salituri@live.com.ar>
2023-03-18 09:50:33 -0300

    18-03-23

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,15 +1,71 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect/* , useState  */} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-//const URL = 'http://localhost:3001/pokemons/';
-import {getAllPokemons} from '../../Redux/actions'
+import {getAllPokemons} from '../../Redux/actions';
+import SearchBar from '../SearchBar/SearchBar'
 
 export default function Home() {
-    /* const dispatch = useDispatch();
-    const allPokemons = useSelector((state) => ) */
+    const dispatch = useDispatch();
+    const allPokemons = useSelector(state => state.allPokemons);
+
+    useEffect(() => {
+        dispatch(getAllPokemons());
+    },[dispatch]);
+
     return(
         <div>
+            <SearchBar />
+            <select name="Filtrar_tipo" id="Filtrar_tipo">
+                <option value="Todos">Mostrar Todos</option>
+                <option value="Bug">Bug</option>
+                <option value="Dark">Dark</option>
+                <option value="Dragon">Dragon</option>
+                <option value="Electric">Electric</option>
+                <option value="Fairy">Fairy</option>
+                <option value="Fighting">Fighting</option>
+                <option value="Fire">Fire</option>
+                <option value="Flying">Flying</option>
+                <option value="Ghost">Ghost</option>
+                <option value="Grass">Grass</option>
+                <option value="Ground">Ground</option>
+                <option value="Ice">Ice</option>
+                <option value="Normal">Normal</option>
+                <option value="Phychic">Phychic</option>
+                <option value="Poison">Poison</option>
+                <option value="Rock">Rock</option>
+                <option value="Shadow">Shadow</option>
+                <option value="Steel">Steel</option>
+                <option value="Unknown">Unknown</option>
+                <option value="Water">Water</option>
+            </select>
+
+            <select name="Filtrar_origen" id="Filtrar_origen">
+                <option value="Mostrar Todos">Mostrar Todos</option>
+                <option value="API">API</option>
+                <option value="Base de Datos">Base de Datos</option>
+            </select>
+
+            <select name="Ordenar" id="Ordenar">
+                <option value="Ascendente">Ascendente</option>
+                <option value="Descendente">Descendente</option>
+            </select>
+
+            <select name="Ordenar_por" id="Ordenar_por">
+                <option value="Nombre">Nombre</option>
+                <option value="Ataque">Ataque</option>
+            </select>
+
             <h1>HomePage</h1>
+            {
+            allPokemons.map((pokemon, index) => {
+                return (
+                    <div key={index}>
+                        <h2>{pokemon.Nombre}</h2>
+                        <img src={pokemon.Imagen} alt={pokemon.Name} />
+                    </div>
+                )
+            })
+            }
         </div>
     )
     
---
+++ client/src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+
+export default function searchBar() {
+    return (
+        <div>
+            <input type="search" />
+            <button>Buscar</button>
+        </div>
+    )
+}
