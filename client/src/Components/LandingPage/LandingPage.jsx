e2bc15681f2d4eaad4dfa38eb94ba5250963addf
pablo-salituri <pablo_salituri@live.com.ar>
2023-03-22 16:17:34 -0300

    boton maso

--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
-//import styles from './LandingPage.module.css'
+import styles from './LandingPage.module.css'
 
 
-export default function LandingPage() {
+export default function LandingPage() {     
 
     useEffect(() => {
         document.body.style.background = "url('https://images4.alphacoders.com/115/1159692.jpg') no-repeat center center fixed";
@@ -12,10 +12,9 @@ export default function LandingPage() {
     },[])
 
     return(
-        <div/*  className = {styles.fondoLanding} */>
-            <h1>Esta es la Landing Page</h1>
+        <div className={styles.div}>
             <Link to='/Home'>
-                <button>INGRESAR</button>
+                <button  className={styles.button}></button>
             </Link>
         </div>
     )
