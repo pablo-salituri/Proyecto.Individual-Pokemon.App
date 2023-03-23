2d76567eff6964c7935d5ff8d1ba4ccf7cdbf090
pablo-salituri <pablo_salituri@live.com.ar>
2023-03-22 21:21:26 -0300

    loading

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import styles from './NavBar.module.css';
+import crea from './crea.png';
+import nosotros from './nosotros.png'
+import { Link } from "react-router-dom";
 
 
 export default function NavBar() {
     return(
-        <div>
-            <h2>Esta es la Navbar</h2>
+        <div className = {styles.div}>
+            <Link to='/FormPage' className={styles.link}>
+                <img className={styles.imglinks} src={crea} alt="Crear_Pokemon" />
+            </Link>
+            <img className={styles.imglinks} src={nosotros} alt="Sobre_Nosotros" />
         </div>
     )
 }
