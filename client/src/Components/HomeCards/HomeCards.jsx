b28797b2bd3c64ffc21dbf6ba11ff341cfd887a8
pablo-salituri <pablo_salituri@live.com.ar>
2023-03-23 10:42:10 -0300

    a reparar post

--- client/src/Components/HomeCards/HomeCards.jsx
+++ client/src/Components/HomeCards/HomeCards.jsx
@@ -4,13 +4,20 @@ import styles from './HomeCards.module.css'
 
 
 
-export default function HomeCards({ID, Nombre, Imagen, Tipo}) {
+export default function HomeCards({ID, Nombre, Imagen, Tipo}) {     //!BREAKPOINT
     return (
         <div className={styles.cards}>
-            <Link to={`/detailPage/${ID}`}>
+            <Link to={`/detailPage/${ID}`} className={styles.cardLink}>
                 <h2 className={styles.cardName}>{Nombre}</h2>
                 <img className={styles.cardImg} src={Imagen} alt={Nombre} />
-                <h5>{Tipo ? Tipo.join(', ') : null}</h5>
+                <div>
+                    <section>
+                        <h4>Tipos</h4>
+                    </section>
+                    <h5>{Tipo[0] ? Tipo[0] : null}</h5>
+                    <h5>{Tipo[1] ? Tipo[1] : null}</h5>
+                    <h5>{Tipo[2] ? Tipo[2] : null}</h5>
+                </div>
             </Link>
         </div>
     )
