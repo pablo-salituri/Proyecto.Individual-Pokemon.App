87f9a9d7cf423dab142f7aea813c5f726eeec1c2
pablo-salituri <pablo_salituri@live.com.ar>
2023-03-25 18:01:34 -0300

    cards

--- client/src/Components/HomeCards/HomeCards.jsx
+++ client/src/Components/HomeCards/HomeCards.jsx
@@ -3,23 +3,25 @@ import { Link } from "react-router-dom";
 import styles from './HomeCards.module.css'
 
 
-export default function HomeCards({ID, Nombre, Imagen, Tipo}) { 
+export default function HomeCards({ID, Nombre, Imagen, Tipo}) {  
     return (
-        <div className={styles.cards}>
-            <Link to={`/detailPage/${ID}`} className={styles.cardLink}>
-                <h2 className={styles.cardName}>{Nombre}</h2>
-                <img className={styles.cardImg} src={Imagen} alt={Nombre} />
-                <div className={styles.cardTipos}>
-                    <section>
-                        <h3 className={styles.cardType}>Tipos</h3>
-                    </section>
-                    <section className={styles.types} style={{'--num-elements': Tipo.filter(tipo => tipo).length}}>
-                        {Tipo.map((tipo, index) => {
-                            return tipo ? <h4 className={styles.cardType} key={index}>{tipo}</h4> : null;
-                        })}
-                    </section>
-                </div>
-            </Link>
+        <div className={styles.borders}>
+            <div className={styles.cards}>
+                <Link to={`/detailPage/${ID}`} className={styles.cardLink}>
+                    <h2 className={styles.cardName}>{Nombre}</h2>
+                    <img className={styles.cardImg} src={Imagen} alt={Nombre} />
+                    <div className={styles.cardTipos}>
+                        <section>
+                            <h3 className={styles.cardType}>Tipos</h3>
+                        </section>
+                        <section className={styles.types} style={{'--num-elements': Tipo.filter(tipo => tipo).length}}>
+                            {Tipo.map((tipo, index) => {
+                                return tipo ? <h4 className={styles.cardType} key={index}>{tipo}</h4> : null;
+                            })}
+                        </section>
+                    </div>
+                </Link>
+            </div>
         </div>
     )
 }
